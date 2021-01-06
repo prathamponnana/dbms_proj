fb7521a3cec3271305cdcee9830022fc0ca44237
PRATHAM PONNANA <46704977+prathamponnana@users.noreply.github.com>
2021-01-06 10:40:41 +0530

    Update add-number.js

--- public/CSU/add-number.js
+++ public/CSU/add-number.js
@@ -9,10 +9,33 @@ showcaseBoxCsu.addEventListener("submit", (e) => {
   successMessage.style.display = "block";
   updateAadhaarBtn.style.visibility = "hidden";
 
+  const adhNo = document.getElementsByClassName("main-inp")[0].value;
+  const name = document.getElementsByClassName("main-inp")[1].value;
+  const dob = document.getElementsByClassName("main-inp")[2].value;
+  const gender = document.getElementsByClassName("main-inp")[3].value;
+  const place = document.getElementsByClassName("main-inp")[4].value;
+  const phone = document.getElementsByClassName("main-inp")[5].value;
+  console.log(adhNo, name, dob, gender, place, phone);
+
+  const person = { adhNo, name, dob, gender, place, phone };
+  console.log(person);
+
+  fetch(`/api/saveperson`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(person),
+  })
+    .then((res) => {
+      console.log(res.status);
+      res.json();
+    })
+    .then((per) => console.log(per))
+    .catch((err) => console.log("Error:",err));
+
   setTimeout(() => {
-    inputGrid.style.display = "grid";
-    successMessage.style.display = "none";
-    updateAadhaarBtn.style.visibility = "visible";
+    window.location.reload();
   }, 3000);
   e.preventDefault();
 });
